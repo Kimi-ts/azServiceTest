e0e4ba6f67659495dbf45faa7d680bb108d8e137
Kimi Raikkonen <r-g@tut.by>
2016-07-01 14:27:41 +0300

    initial-commit

---
+++ AudioWeb/App_Start/BundleConfig.cs
@@ -0,0 +1,12 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace AudioWeb
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+        }
+    }
+}
