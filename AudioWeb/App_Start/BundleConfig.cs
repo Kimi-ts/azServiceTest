46d89e0ddb72102e4e35865cea106fa0706fe09d
Kimi Raikkonen <r-g@tut.by>
2016-07-05 14:29:00 +0300

    added-bower.json

--- AudioWeb/App_Start/BundleConfig.cs
+++ AudioWeb/App_Start/BundleConfig.cs
@@ -7,7 +7,14 @@ namespace AudioWeb
     {
         public static void RegisterBundles(BundleCollection bundles)
         {
+            //bundles.Add(new ScriptBundle("~/bundles/AudioApp")
+            //    .IncludeDirectory("~/Scripts/Controllers", "*.js")
+            //    .IncludeDirectory("~/Scripts/Factories", "*.js")
+            //    .Include("~/Scripts/audioApp.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/AudioApp")
+                .Include("~/bower_components/angular/angular.min.js")
+                .Include("~/bower_components/angular-audio/app/angular.audio.js")
                 .IncludeDirectory("~/Scripts/Controllers", "*.js")
                 .IncludeDirectory("~/Scripts/Factories", "*.js")
                 .Include("~/Scripts/audioApp.js"));
